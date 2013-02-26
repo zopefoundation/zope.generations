11f8c0a4e732e0b794852b1094b33b1ef2bb8805
Stephan Richter <stephan.richter@gmail.com>
2013-02-25 20:19:11 -0500

    Preparing release 4.0.0a1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.generations',
-      version = '4.0.0a1.dev',
+      version='4.0.0a1',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope application schema generations',
